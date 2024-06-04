f045c7fd5986f46ca697d85d44490d74c6b2835a
devindawanasinghe <devindawanasinghe@gmail.com>
2024-06-04 23:58:50 +0530

    admin panel get user details

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const app = express()
+const mysql = require("mysql");
+const cors = require("cors");
+
+//const port = 3000
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    user:"root",
+    host:"localhost",
+    password:"",
+    database:"main",
+});
+
+//show in browser
+// app.get('/', (req, res) => {
+//   res.send('Dewinda wanasinghe!')
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+//get data in database (show in browser)
+app.get('/users', (req, res) => {
+    db.query("SELECT * FROM users",(err, result) => {
+        if (err){
+            console.log(err);
+          
+        }else{
+            res.send(result);
+        }
+    });
+  });
+
+//data insert database
+app.post("/create", (req, res) =>{
+  const name = req.body.name;
+  const password = req.body.password;
+  const email = req.body.email;
+  const phone = req.body.phone;
+
+  db.query(
+    "INSERT INTO users (name, password, email, phone) VALUES (?,?,?,?)",
+    [name, password, email, phone],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }else{
+        res.send("You have registered successfully!");
+      }
+    }
+  );
+
+});
+
+//Running terminal
+app.listen(3001, () => {
+    console.log(`Example app listening on port 3001`);
+  });
