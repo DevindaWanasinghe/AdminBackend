12504180a105e07a4be11a7b9ef98ed21030ab8f
devindawanasinghe <devindawanasinghe@gmail.com>
2024-07-26 21:02:02 +0530

    adminpanal

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const mysql = require("mysql");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: './.env'});
 
 //const port = 3000
 
@@ -9,31 +12,40 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    user:"root",
-    host:"localhost",
-    password:"",
-    database:"main",
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
 });
 
+//checking my sql database connection,
+db.connect( (error) => {
+    if(error){
+        console.log(error)
+    }else{
+        console.log("MySql Connected...")
+    }
+})
+
 //show in browser
-// app.get('/', (req, res) => {
-//   res.send('Dewinda wanasinghe!')
-// });
+app.get('/', (req, res) => {
+    res.send('Dewinda wanasinghe!')
+});
 
 // app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
+//    console.log(`Example app listening on port ${port}`)
 // })
 
 //get data in database (show in browser)
 app.get('/users', (req, res) => {
-    db.query("SELECT * FROM users",(err, result) => {
-        if (err){
-            console.log(err);
-          
-        }else{
-            res.send(result);
-        }
-    });
+      db.query("SELECT * FROM users",(err, result) => {
+            if (err){
+                console.log(err);
+
+            }else{
+                res.send(result);
+            }
+      });
   });
 
 //data insert database
@@ -46,18 +58,18 @@ app.post("/create", (req, res) =>{
   db.query(
     "INSERT INTO users (name, password, email, phone) VALUES (?,?,?,?)",
     [name, password, email, phone],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      }else{
-        res.send("You have registered successfully!");
+      (err, result) => {
+          if (err) {
+            console.log(err);
+          }else{
+            res.send("You have registered successfully!");
+          }
       }
-    }
   );
 
 });
 
 //Running terminal
 app.listen(3001, () => {
-    console.log(`Example app listening on port 3001`);
+      console.log(`Example app listening on port 3001`);
   });
